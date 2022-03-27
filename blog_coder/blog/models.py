6b89ce7c81f24d8b2246600538b49670dae498cf
Tyler Cruz <tyzurc97@gmail.com>
2022-03-27 20:46:42 -0300

    Primer modelo creado y registrado

--- blog_coder/blog/models.py
+++ blog_coder/blog/models.py
@@ -1,3 +1,9 @@
+from pickle import TRUE
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=TRUE)
+    titulo = models.CharField(max_length=60)
+    contenido = models.TextField()
